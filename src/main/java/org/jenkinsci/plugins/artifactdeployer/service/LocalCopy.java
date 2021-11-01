325a0a59e5d392cf3208f7cd89e11b847d391979
Kai Broszat <kai.broszat@hlp.de>
2021-11-01 09:03:45 +0100

    Replace com.atlassian.ant-filesystem-tasks with commons-io

--- src/main/java/org/jenkinsci/plugins/artifactdeployer/service/LocalCopy.java
+++ src/main/java/org/jenkinsci/plugins/artifactdeployer/service/LocalCopy.java
@@ -22,69 +22,52 @@
  */
 package org.jenkinsci.plugins.artifactdeployer.service;
 
-import com.atlassian.ant.tasks.CopyWithPerms;
-import org.apache.tools.ant.BuildException;
+import org.apache.commons.io.FileUtils;
 import org.apache.tools.ant.types.FileSet;
+import org.apache.tools.ant.types.Resource;
+import org.apache.tools.ant.types.resources.FileResource;
 import org.jenkinsci.plugins.artifactdeployer.ArtifactDeployerException;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
- * @author Gregory Boissinot
+ * @author Kai Broszat
  */
 public class LocalCopy {
 
-    public List<File> copyAndGetNumbers(FileSet fileSet, boolean flatten, File target) throws ArtifactDeployerException {
+    public List<File> copyAndGetNumbers(FileSet fileSet, boolean flatten, File targetDir) throws ArtifactDeployerException {
+        List<File> deployedFiles = new ArrayList<>(fileSet.size());
 
-        class CopyImpl extends CopyWithPerms {
+        File baseDir = fileSet.getDir();
+        Iterator<Resource> iter = fileSet.iterator();
+        while (iter.hasNext()) {
+            FileResource resource = (FileResource) iter.next();
+            File source = resource.getFile();
+            File dest = computeDestFile(baseDir, targetDir, source, flatten);
 
-            private final List<File> deployedFiles = new ArrayList<File>();
-
-            //private int copySize;
-
-            public CopyImpl() {
-                setProject(new org.apache.tools.ant.Project());
-            }
-
-            @Override
-            protected void doFileOperations() {
-                //copySize = super.fileCopyMap.size();
-                for (Object tabOb : super.fileCopyMap.values()) {
-                    String[] tab = (String[]) tabOb;
-                    for (String f : tab) {
-                        deployedFiles.add(new File(f));
-                    }
-                }
-                super.doFileOperations();
-            }
-
-            //public int getNumCopied() {
-            //    return copySize;
-            //}
-
-            public List<File> getDeployedFiles() {
-                return deployedFiles;
+            try {
+                // original code included empty directories - this code doesn't
+                FileUtils.copyFile(source, dest, true, StandardCopyOption.COPY_ATTRIBUTES);
+                deployedFiles.add(dest);
+            } catch (IOException ioExceptio) {
+                throw new ArtifactDeployerException("Error on copying file.", ioExceptio);
             }
         }
-
-        CopyImpl copyTask = new CopyImpl();
-        copyTask.setTodir(target);
-        copyTask.addFileset(fileSet);
-        //Do not overwrite file: the plugin has the delete content directory previously
-        copyTask.setOverwrite(false);
-        copyTask.setIncludeEmptyDirs(true);
-        copyTask.setFlatten(flatten);
-        copyTask.setPreserveLastModified(true);
-        copyTask.setPreservePermissions(true);
-
-        try {
-            copyTask.execute();
-        } catch (BuildException be) {
-            throw new ArtifactDeployerException("Error on copying file.", be);
-        }
-        return copyTask.getDeployedFiles();
+        return deployedFiles;
     }
 
+    private File computeDestFile(File baseDir, File targetDir, File sourceFile, boolean flatten) {
+        if(flatten) {
+            return new File(targetDir, sourceFile.getName());
+        } else {
+            Path relativePathOfSourceFile = sourceFile.toPath().relativize(baseDir.toPath());
+            return new File(targetDir, relativePathOfSourceFile.toString());
+        }
+    }
 }
